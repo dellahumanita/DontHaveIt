3eabbcef74cb113b5929760d4c37f6164cd27733
Della <humanita.1999@gmail.com>
2021-01-17 00:42:18 -0700

    added search bar

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { TextField, Button } from '@material-ui/core';
 
 export default function Search() {
- return(
+
+ return (
 	<div
-		className="">
-		<h2>Search</h2>
+		className="container mx-auto">
+			<div className="object-center">
+				<h2>Search</h2>
+			{/* <FormControl>
+				<InputLabel htmlFor="my-input">Enter an ingredient</InputLabel>
+				<Input id="my-input" aria-describedby="my-helper-text" />
+				<FormHelperText id="my-helper-text">Find an alternative to an ingredient you are looking for!</FormHelperText>
+			</FormControl> */}
+
+				<TextField id="filled-basic" label="Search" variant="filled" />
+				<div>
+					<Button variant="contained" color="teal">
+						Find Substitute
+					</Button>
+				</div>
+			</div>
+
 	</div>
 	);
 }
